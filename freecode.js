0bea5e06711f43e7f24311d8bcbe1439cc2eb194
Rashid Jama <rashidjama5@gmail.com>
2020-12-01 16:27:55 -0800

    binary tree search insertion method

--- freecode.js
+++ freecode.js
@@ -156,3 +156,69 @@
 // console.log(q)
 
 // console.log(q.print())
+
+
+// Binary Tree Search
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTrees {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if(this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while(true) {
+        if(value < current.value) {
+          if(current.left === null ) {
+            current.left = newNode;
+            return this;
+          } else {
+            current = current.left;
+          }
+        }
+        
+        else if(value > current.value) {
+          if(current.right === null ) {
+            current.right = newNode;
+            return this;
+          } else {
+            current = current.right;
+          }
+        }
+      }
+    }
+}
+
+}
+
+let tree = new BinarySearchTrees();
+tree.insert(10)
+tree.insert(6)
+tree.insert(5)
+tree.insert(3)
+console.log(tree)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
